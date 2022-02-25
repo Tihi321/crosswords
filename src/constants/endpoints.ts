f1b5d69b6bcd518d0fe838345b7965561877e082
Tihomir Selak <tknox.dr@gmail.com>
2022-02-25 21:59:43 +0100

    CS-39: Update api links

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,8 +1,4 @@
 export enum Endpoints {
-  General = "https://api.tihomir-selak.from.hr/rest/krizaljka/opcenito/index.json",
-  Words = "https://api.tihomir-selak.from.hr/cdn/json/random-vocabulary-words.json",
-  CroatianWords = "https://api.tihomir-selak.from.hr/cdn/json/random-vocabulary-words-hr.json",
-  FirstNames = "https://api.tihomir-selak.from.hr/cdn/json/random-first-names.json",
-  LastNames = "https://api.tihomir-selak.from.hr/cdn/json/random-last-names.json",
-  Quotes = "https://api.tihomir-selak.from.hr/cdn/json/quotes.json",
+  Words = "https://cdn.tihomir-selak.from.hr/api/random-vocabulary-words.json",
+  CroatianWords = "https://cdn.tihomir-selak.from.hr/api/random-vocabulary-words-hr.json",
 }
